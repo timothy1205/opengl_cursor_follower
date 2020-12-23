ed8b1c336f49009dd151bd0afeda77548b81255a
timothy1205 <timothy120500@gmail.com>
2020-12-23 00:49:07 -0700

    Setting up the main overlay window/render

---
+++ Renderer.cpp
@@ -0,0 +1,14 @@
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "Renderer.h"
+
+Renderer::Renderer() {
+
+}
+
+Renderer::~Renderer() = default;
+
+void Renderer::setFollowerPosition(float x, float y) {
+
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,75 @@
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
+#include <iostream>
+#include <csignal>
+#include <X11/Xlib.h>
+
+void getVirtualDesktopDimensions(int* width, int* height);
+
+namespace {
+    bool interrupted = false;
+}
 
 int main() {
+    // Handle program interruption
+    std::signal(SIGINT, [](int) { interrupted = true; });
+
     glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    glfwWindowHint(GLFW_FOCUSED, GLFW_FALSE);
+    glfwWindowHint(GLFW_FOCUS_ON_SHOW, GLFW_FALSE);
+    glfwWindowHint(GLFW_FLOATING, GLFW_TRUE);
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+
+    /* Wait for https://github.com/glfw/glfw/pull/1568 to be released in next GLFW verison
+     * glfwWindowHint(GLFW_MOUSE_PASSTHROUGH, GLFW_TRUE);
+     * TODO: Build latest version myself so I can continue
+     */
+
+    // Create window based on virtual desktop dimensions
+    int width, height;
+    getVirtualDesktopDimensions(&width, &height);
+    GLFWwindow* window = glfwCreateWindow(width, height, "opengl_cursor_follower", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    glfwSetWindowPos(window, 0, 0);
+
+    // Main application loop
+    while (!interrupted) {
+
+    }
+
     glfwTerminate();
+    return 0;
+}
+
+void getVirtualDesktopDimensions(int* width, int* height) {
+    int monitorCount;
+    GLFWmonitor** monitors = glfwGetMonitors(&monitorCount);
+
+    if (!monitors) {
+        std::cerr << "No monitors found..." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    *width = 0;
+    *height = 0;
+
+    int xPos, yPos, tWidth, tHeight;
+    for (int i = 0; i < monitorCount; i++) {
+        glfwGetMonitorWorkarea(monitors[i], &xPos, &yPos, &tWidth, &tHeight);
+
+        int right = xPos + tWidth;
+        if (right > *width) {
+            *width = right;
+        }
+        int bottom = yPos + tHeight;
+        if (bottom > *height) {
+            *height = bottom;
+        }
+    }
 }
