080842cca3b1945dd35f1058aa7df83d0d300c55
timothy1205 <timothy120500@gmail.com>
2020-12-24 15:36:31 -0600

    Created window in fullscreen windowed mode

--- main.cpp
+++ main.cpp
@@ -14,9 +14,25 @@ int main() {
     std::signal(SIGINT, [](int) { interrupted = true; });
 
     glfwInit();
+
+    /*
+     * Version 3.4 is necessary for GLFW_MOUSE_PASSTHROUGH, however specifying it
+     * as such causes the window to not be created...
+     */
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /*
+     * Window managers need to disable managing for window
+     * Ex: bspc rule -a opengl_cursor_follower manage=off
+     */
+
+    const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+    glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+    glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+    glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
 
     glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
@@ -25,11 +41,13 @@ int main() {
     glfwWindowHint(GLFW_FLOATING, GLFW_TRUE);
     glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
     glfwWindowHint(GLFW_MOUSE_PASSTHROUGH, GLFW_TRUE);
+    glfwWindowHint(GLFW_CENTER_CURSOR, GLFW_FALSE);
 
     // Create window based on virtual desktop dimensions
     int width, height;
     getVirtualDesktopDimensions(&width, &height);
-    GLFWwindow* window = glfwCreateWindow(width, height, "opengl_cursor_follower", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "opengl_cursor_follower", glfwGetPrimaryMonitor(), NULL);
+    glfwSetWindowPos(window, -500, 100);
     glfwMakeContextCurrent(window);
     glfwSetWindowPos(window, 0, 0);
 
