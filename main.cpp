e653b4f513c11be6c449a8736ddeba4e1be387e3
timothy1205 <timothy120500@gmail.com>
2021-06-08 13:47:26 -0600

    Added argument parsing library

--- main.cpp
+++ main.cpp
@@ -1,16 +1,34 @@
-#include <iostream>
 #include <csignal>
+#include <iostream>
 #include <unistd.h>
+
+#include <argparser.hpp>
+
 #include "config.h"
 #ifdef USE_X11
 #include "x11_WindowRenderer.h"
 #endif
 
 namespace {
+    bool showHelp = false;
+
     bool interrupted = false;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char const* argv[]) {
+    stypox::ArgParser parser{std::make_tuple(
+        stypox::HelpSection{"\nOptions:"},
+
+        stypox::SwitchOption{"help", showHelp, stypox::args("-h", "--help"),  "Print help"}
+    ), "OpenGL Cursor Follower"};
+
+    parser.parse(argc, argv);
+
+    if (showHelp) {
+        std::cout << parser.help();
+        return EXIT_SUCCESS;
+    }
+
     // Handle program interruption
     std::signal(SIGINT, [](int) { interrupted = true; });
 
@@ -22,5 +40,4 @@ int main(int argc, char** argv) {
         windowRenderer.render();
         usleep((1/60) * 1000000);
     }
-
 }
